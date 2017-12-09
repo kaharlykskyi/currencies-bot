18933b7fc4234c5ed2c5e710020eace560d8a482
Misha Kaharlykskyi <mishakagar@gmail.com>
2017-12-10 00:38:25 +0200

    TG hook fix

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -20,34 +20,28 @@ class ExampleController extends Controller
         $text = $result["message"]["text"]; //Текст сообщения
         $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
         $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-        $keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
+        $keyboard = [["Котик"], ["Гифка с котиком"]]; //Клавиатура
 
         if($text){
             $reply = '';
             if ($text == "/start") {
-                $reply = "Добро пожаловать в бота!";
+                $reply = "Привет, $name! Вот список доступных команд:";
                 $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
                 $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
             }elseif ($text == "/help") {
-                $reply = "Информация с помощью.";
+                $reply = "Список доступных команд:";
                 $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-            }elseif ($text == "Картинка") {
+            }elseif ($text == "Котик") {
                 $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
                 $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-            }elseif ($text == "Гифка") {
+            }elseif ($text == "Гифка с котиком") {
                 $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
                 $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-            }elseif ($text == "Последние статьи") {
-                $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-                foreach ($html->channel->item as $item) {
-                    $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-                }
-                $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
             }else{
-                $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-                $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
+                $reply = "Команды \"<b>".$text."</b>\" не существует.";
+                $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'text' => $reply ]);
             }
-        }else{
+        }else {
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
         }
     }
